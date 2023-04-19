697f29ad5669c72e15d5e51ada9722463c882459
Romans Špiļaks <romans.spilaks@gmail.com>
2023-04-19 21:24:58 +0300

    Add section Location. Add image and text for section Location. Add regulations text and image. Add hover and after pseudoelement to Location section image.

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,8 +3,9 @@
 const navMenu = document.querySelector('.nav-menu');
 const navToggle = document.querySelector('.nav-toggle');
 const navLink = document.querySelectorAll('.nav-link');
-const burgerLineTop = document.querySelector('.burger-line-top');
-const burgerLineBottom = document.querySelector('.burger-line-bottom');
+
+//  const burgerLineTop = document.querySelector('.burger-line-top');
+//  const burgerLineBottom = document.querySelector('.burger-line-bottom');
 
 function toggleBurger() {
   const width = window.innerWidth;
@@ -14,15 +15,15 @@ function toggleBurger() {
     navMenu.setAttribute('data-visible', true);
     navMenu.setAttribute('aria-expanded', true);
     navMenu.classList.remove('hidden');
-   // burgerLineBottom.style.y = ('60');
-   // burgerLineTop.style.y = ('60');
+    // burgerLineBottom.style.y = ('60');
+    // burgerLineTop.style.y = ('60');
   } else if (visibility === 'true') {
     navToggle.setAttribute('aria-expanded', false);
     navMenu.setAttribute('data-visible', false);
     navMenu.setAttribute('aria-expanded', false);
     navMenu.classList.add('hidden');
-  //  burgerLineBottom.style.y = ('20');
-   // burgerLineTop.style.y = ('55');
+    //  burgerLineBottom.style.y = ('20');
+    // burgerLineTop.style.y = ('55');
   }
 }
 
